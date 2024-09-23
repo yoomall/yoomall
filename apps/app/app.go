865ed0766acddd20a41bc0505917d3b6a179d103
suke <suke971219@gmail.com>
2024-09-23 11:25:09 +0800

    /

--- apps/app/app.go
+++ apps/app/app.go
@@ -1,6 +1,8 @@
 package app
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 	"github.com/spf13/viper"
 	"lazyfury.github.com/yoomall-server/core"
@@ -26,4 +28,13 @@ func (d *DefaultApp) Middleware() []gin.HandlerFunc {
 
 func (d *DefaultApp) Register(router *core.RouterGroup) {
 
+	// health check
+	router.WithDoc(&core.DocItem{
+		Method: http.MethodGet,
+		Tag:    "app",
+		Path:   "/health",
+	}).GET("/health", func(ctx *gin.Context) {
+		// TODO:collect other info if need
+		ctx.JSON(200, map[string]any{"ok": true})
+	})
 }
