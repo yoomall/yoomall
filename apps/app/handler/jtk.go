2574b1dc7708c823b54583150238037f97c5d094
suke <suke971219@gmail.com>
2024-09-26 14:22:17 +0800

    /

--- apps/app/handler/jtk.go
+++ apps/app/handler/jtk.go
@@ -38,17 +38,6 @@ func (j *JtkHandler) Register(router *core.RouterGroup) {
 	}).GET("", j.jtk)
 }
 
-// 聚推客接口 godoc
-//
-//	@Summary		获取聚推客接口数据
-//	@Description	聚推客接口
-//	@Tags			/jtk
-//	@Accept			json
-//	@Produce		json
-//	@Param			path query string true "接口路径"
-//	@Param			method query string true "请求方法"
-//	@Param			...params query string false "请求参数/其他参数都是动态的参考聚推客开发文档/ swagger 不支持，请使用 apipost 工具调试"
-//	@Router			/jtk [get]
 func (j *JtkHandler) jtk(ctx *gin.Context) {
 	var query map[string]string = make(map[string]string)
 	ctx.ShouldBindQuery(&query)
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,20 +1,5 @@
 package main
 
-// @title						Nunu Example API
-// @version					1.0.0
-// @description				This is a sample server celler server.
-// @termsOfService				http://swagger.io/terms/
-// @contact.name				API Support
-// @contact.url				http://www.swagger.io/support
-// @contact.email				support@swagger.io
-// @license.name				Apache 2.0
-// @license.url				http://www.apache.org/licenses/LICENSE-2.0.html
-// @host						localhost:8900
-// @securityDefinitions.apiKey	Bearer
-// @in							header
-// @name						Authorization
-// @externalDocs.description	OpenAPI
-// @externalDocs.url			https://swagger.io/resources/open-api/
 func main() {
 	server := NewApp()
 	server.Start()
