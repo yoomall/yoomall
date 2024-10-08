091a92f5f922ddc6806241b779f09869e391ac3a
suke <suke971219@gmail.com>
2024-10-08 09:09:10 +0800

    proxy http

--- apps/app/app.go
+++ apps/app/app.go
@@ -6,6 +6,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/spf13/viper"
 	"lazyfury.github.com/yoomall-server/core"
+	"lazyfury.github.com/yoomall-server/core/helper/utils"
 	coremiddleware "lazyfury.github.com/yoomall-server/core/middleware"
 )
 
@@ -36,5 +37,9 @@ func (d *DefaultApp) Register(router *core.RouterGroup) {
 	}).GET("/health", func(ctx *gin.Context) {
 		// TODO:collect other info if need
 		ctx.JSON(200, map[string]any{"ok": true})
+	})
+
+	router.GET("/proxy", func(ctx *gin.Context) {
+		utils.ProxyRequest(ctx)
 	})
 }
