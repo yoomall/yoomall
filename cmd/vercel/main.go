479b19db1581c7966dfdcc1d426f42f8a2b74c1d
suke <suke971219@gmail.com>
2024-11-01 15:23:49 +0800

    /

--- cmd/vercel/main.go
+++ cmd/vercel/main.go
@@ -3,18 +3,15 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"time"
 	"yoomall/api"
 )
 
 func main() {
-	start := time.Now()
 	// 测试 vercel 服务配置是否正常
 	server := http.NewServeMux()
 	server.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		api.Handler(w, r)
 	})
 	fmt.Println("start http server on port: http://127.1:8900")
-	fmt.Println("end at:", time.Now().Sub(start))
 	http.ListenAndServe(":8900", server)
 }
